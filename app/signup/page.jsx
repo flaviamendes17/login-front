19e353d045e9fb5784b9d6fac1dac3ff5947488f
Flavia Mendes <flavinharamendes@gmail.com>
2025-03-20 11:42:17 -0300

    feat: password conference and finalization

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,10 +1,39 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation"; // Importa o hook useRouter
 import styles from "../../styles/Home.module.css";
-import { useRouter } from 'next/navigation';
 
 export default function SignUp() {
-    const router = useRouter();
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+    const [match, setMatch] = useState(null);
+    const [error, setError] = useState(null);
+    const router = useRouter(); 
+
+    const handleCheck = () => {
+        if (input1.length === 0) {
+            setMatch(false);
+            setError("Coloque uma senha");
+            return; 
+        } else if (input2.length === 0) {
+            setMatch(false);
+            setError("Confirme a senha");
+            return; 
+        } else if (input1.length < 6) {
+            setMatch(false);
+            setError("A senha deve ter pelo menos 6 caracteres");
+            return; 
+        } else if (input1 !== input2) {
+            setMatch(false);
+            setError("As senhas não são iguais");
+            return; 
+        }
+
+        setMatch(true);
+        setError("Senha cadastrada com sucesso!");
+        router.push("/profile"); 
+    };
 
     return (
         <div className={styles.container}>
@@ -15,11 +44,13 @@ export default function SignUp() {
                 <h1 className={styles.title}>SIGN UP</h1>
                 <input className={styles.inputField} type="text" placeholder="Nome" />
                 <input className={styles.inputField} type="email" placeholder="Email" />
-                <input className={styles.inputField} type="password" placeholder="Senha" />
-                <input className={styles.inputField} type="password" placeholder="Confirme a senha" />
+                <input className={styles.inputField} type="password" placeholder="Senha" value={input1} onChange={(e) => setInput1(e.target.value)} />
+                <input className={styles.inputField} type="password" placeholder="Confirme a senha" value={input2} onChange={(e) => setInput2(e.target.value)} />
                 <input className={styles.inputField} type="date" placeholder="Data de Nascimento" />
                 <input className={styles.inputField} type="text" placeholder="Curso" />
-                <button className={styles.signUpButton} onClick={() => router.push('/profile')}>Cadastrar</button>
+                
+                <button className={styles.signUpButton} onClick={handleCheck}>Cadastrar</button>
+                {match !== null && <p>{error}</p>}
             </div>
         </div>
     );
