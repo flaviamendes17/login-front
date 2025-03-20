b6cfc53e385728731353c5416648dcec948721f8
Flavia Mendes <flavinharamendes@gmail.com>
2025-03-20 09:20:09 -0300

    feat: profile features

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import "./profile.css";
 
 export default function Profile() {
+    const router = useRouter(); 
+
     return (
         <div className="profileContainer">
             <div className="profileCard">
@@ -14,7 +19,23 @@ export default function Profile() {
                     <div className="card">flavia.r.mendes@aluno.senai.br</div>
                     <div className="card">Aniversário: 17/03/2008</div>
                     <div className="card">Técnico em desenvolvimento de sistemas 1</div>
+                    <div className="card">Técnologias estudadas</div>
                 </div>
+                <div className="competencias">
+                    <img src="nodejs.png" alt="nodejs" />
+                    <img src="html.png" alt="html" />
+                    <img src="css.png" alt="css" />
+                    <img src="postgree.png" alt="postgrees" />
+                    <img src="postman.png" alt="postman" />
+                    <img src="nextjs.png" alt="nextjs" />
+                    <img src="js.png" alt="javascript" />
+                </div>
+                <button 
+                    className="backToLoginButton" 
+                    onClick={() => router.push('/')}
+                >
+                    Voltar para Login
+                </button>
             </div>
         </div>
     );
